779c890f1ebb534d845c3c685b95f087c42e8760
tom99715 <108880340+tom99715@users.noreply.github.com>
2024-10-27 17:10:18 +0000

    updated mac setup

--- nvim/lua/plugins/nerdtree.lua
+++ nvim/lua/plugins/nerdtree.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       vim.cmd([[
         autocmd StdinReadPre * let s:std_in=1
-        autocmd VimEnter * NERDTree
+        autocmd VimEnter * if argc() == 0 && !exists("g:lazy_nvim_opening") | NERDTree | endif
       ]])
       vim.g.NERDTreeGitStatusWithFlags = 1
       vim.g.NERDTreeIgnore = { '^node_modules$' }
